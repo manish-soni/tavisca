0c4f206e1df2bf57824b755f2618d716867a2b34
manish <mani151092@gmail.com>
2020-05-01 03:33:24 +0530

    added components for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlightComponent } from './flight/flight.component';
 import { FlightDetailsComponent } from './flight-details/flight-details.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { TrainsComponent } from './trains/trains.component';
+import { BusComponent } from './bus/bus.component';
+
 const routes: Routes = [
   { path: 'flight', component: FlightComponent },
-  { path: 'flight-details', component: FlightDetailsComponent },
   { path: '',   redirectTo: '/flight', pathMatch: 'full' }, // redirect to `flight component`
+  { path: 'hotel', component: HotelsComponent },
+  { path: 'trains', component: TrainsComponent },
+  { path: 'buses', component: BusComponent },
+  { path: 'flight-details', component: FlightDetailsComponent },
+ 
   { path: '**', component: FlightComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { FlightComponent } from './flight/flight.component';
 import { FlightDetailsComponent } from './flight-details/flight-details.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HotelsComponent } from './hotels/hotels.component';
+import { TrainsComponent } from './trains/trains.component';
+import { BusComponent } from './bus/bus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FlightComponent,
-    FlightDetailsComponent
+    FlightDetailsComponent,
+    HotelsComponent,
+    TrainsComponent,
+    BusComponent
   ],
   imports: [
     BrowserModule,
